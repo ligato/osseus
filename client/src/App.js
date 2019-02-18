f51b1fad4d2082851fbd867b438e7678c7daac5c
Kevin-Velasquez <kevinx23x@yahoo.com>
2019-02-18 11:06:02 -0800

    populated plugin cards and created organization by type
    Signed-off-by: Kevin-Velasquez <kevinx23x@yahoo.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,51 @@
 import React, { Component } from "react";
-import PluginPicker from './PluginPicker';
-import PluginPalette from './PluginPalette';
+
+import PluginPickerRPC from './pluginpicker/PluginPicker_RPC';
+import PluginPickerDS from "./pluginpicker/PluginPicker_DS";
+import PluginPickerLOG from "./pluginpicker/PluginPicker_LOG";
+import PluginPickerHTH from "./pluginpicker/PluginPicker_HTH";
+import PluginPickerMISC from "./pluginpicker/PluginPicker_MISC";
+
 import DraggablePlugins from './DraggablePlugins';
+
+import PluginPalette from './PluginPalette'
 import "./styles/App.css";
 
 class App extends Component {
   render() {
     return (
-        <div>         
-            <PluginPicker>
-                <DraggablePlugins />      
-            </PluginPicker>
-            <PluginPalette />
+        <div> 
+            <PluginPickerRPC>
+                <DraggablePlugins pluginName = "REST API" />   
+                <DraggablePlugins pluginName = "GRPC" />    
+                <DraggablePlugins pluginName = "PROMETHEUS" />          
+            </PluginPickerRPC>
+
+            <PluginPickerDS>
+                <DraggablePlugins pluginName = "ETCD" /> 
+                <DraggablePlugins pluginName = "REDIS" /> 
+                <DraggablePlugins pluginName = "CASSANDRA" /> 
+                <DraggablePlugins pluginName = "CONSUL" /> 
+            </PluginPickerDS>
+
+            <PluginPickerLOG>
+                <DraggablePlugins pluginName = "LOGRUS" />   
+                <DraggablePlugins pluginName = "LOG MNGR" />             
+            </PluginPickerLOG>
+
+            <PluginPickerHTH>
+                <DraggablePlugins pluginName = "STTS CHECK" />   
+                <DraggablePlugins pluginName = "PROBE" />             
+            </PluginPickerHTH>
+
+            <PluginPickerMISC>
+                <DraggablePlugins pluginName = "KAFKA" />   
+                <DraggablePlugins pluginName = "DATASYNC" />     
+                <DraggablePlugins pluginName = "IDX MAP" />   
+                <DraggablePlugins pluginName = "SRVC LABEL" />    
+                <DraggablePlugins pluginName = "CONFIG" />         
+            </PluginPickerMISC>
+            <PluginPalette></PluginPalette>
         </div>
     );
   }
--- client/src/DraggablePlugins.js
+++ client/src/DraggablePlugins.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import "./styles/App.css";
+import "./styles/Plugincard.css";
 
 /*This component represented the plugin cards. This compenent is
 * a child of Plugin Picker initially. Once drag and drop is implemented
 * presumedly each instance of a "plugin" could change from being a child
 * of the picker to being a child of palette once its is dragged over.
 */ 
-const DraggablePlugins = () => {
+const DraggablePlugins = (props) => {
     return (
         <div className="body">
             <div className="card">
                 <img src="https://i.ibb.co/r6VkQ19/Wordpress-Movie-Theme-Free.png" alt="Avatar"></img>
-                <div className="container">
-                    <p className="whitetext">Plugin</p>
+                <div>
+                    <p className="cardtext">{props.pluginName}</p>
                 </div>
             </div>
         </div>
