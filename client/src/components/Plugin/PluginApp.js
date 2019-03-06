a296d965c0af212dde9413003ce8218f2ca74e9d
Kevin <kevinx23x@yahoo.com>
2019-03-06 02:56:31 -0800

    Fixed pure percentages and looped components
    Signed-off-by: Kevin <kevinx23x@yahoo.com>

--- client/src/components/Plugin/PluginApp.js
+++ client/src/components/Plugin/PluginApp.js
@@ -3,7 +3,8 @@ import PluginPicker from './PluginPicker';
 import DraggablePlugins from './DraggablePlugins';
 import PluginPalette from './PluginPalette';
 
-let pluginArray =  new Uint8Array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+let pluginPickedArray =  new Uint8Array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+let offsets = [[0],[3],[7],[9],[11],[17]];
 
 /*
 * The class and its member function are used to recieve data from children; currently it captures 
@@ -21,7 +22,13 @@ class PluginApp extends React.Component {
         super();
         this.handleData = this.handleData.bind(this);
         this.state = {
-          clickedIndex: null
+          clickedIndex: null,
+          pluginNames: ['REST API',  'GRPC',       'PROMETHEUS',   'ETCD',      
+                        'REDIS',     'CASSANDRA',  'CONSUL',       'LOGRUS',     
+                        'LOG MNGR',  'STTS CHECK', 'PROBE',        'KAFKA',     
+                        'DATASYNC',  'IDX MAP',    'SRVC LABEL',   'CONFIG'],
+
+          sentInNames: ['RPC', 'Data Store', 'Logging', 'Health', 'Misc.']
         };
     }
     
@@ -29,61 +36,46 @@ class PluginApp extends React.Component {
         this.setState({
           clickedIndex: index
         });
-        pluginArray[index] = !pluginArray[index];
+        pluginPickedArray[index] = !pluginPickedArray[index];
     }
+
     render() {
-        /*
-        * This huge block of code could likely be compressed into objects but for testing this will be
-        * openly displayed exactly where its being used for testing and understanding purposes.
-        *
-        * This block of code outlines the structure of whats a child of what, what props are being passed where
-        * etc.
-        */ 
         return (
             <div>
-                <PluginPicker sentInStyle = "split leftRPC"  sentInName="RPC"        sentInArray={pluginArray.subarray(0,3)}>
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "REST API"  grouping = "RPC"  id={0} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "GRPC"      grouping = "RPC"  id={1} />    
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "PROMETHEUS"grouping = "RPC"  id={2} />          
-                </PluginPicker>
-                <PluginPicker sentInStyle = "split leftDS"   sentInName="Data Store" sentInArray={pluginArray.subarray(3,7)}>
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "ETCD"      grouping = "DS"   id={3} /> 
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "REDIS"     grouping = "DS"   id={4} /> 
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "CASSANDRA" grouping = "DS"   id={5} /> 
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "CONSUL"    grouping = "DS"   id={6} />          
-                </PluginPicker>
-                <PluginPicker sentInStyle = "split leftLOG"  sentInName="Logging"    sentInArray={pluginArray.subarray(7,9)}>
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "LOGRUS"    grouping = "LOG"  id={7} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "LOG MNGR"  grouping = "LOG"  id={8} />           
-                </PluginPicker>
-                <PluginPicker sentInStyle = "split leftHTH"  sentInName="Health"     sentInArray={pluginArray.subarray(9,11)}>
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "STTS CHECK"grouping = "HTH"  id={9} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "PROBE"     grouping = "HTH"  id={10} />             
-                </PluginPicker>
-                <PluginPicker sentInStyle = "split leftMISC" sentInName="Misc."      sentInArray={pluginArray.subarray(11,17)}>
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "KAFKA"     grouping = "MISC" id={11} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "DATASYNC"  grouping = "MISC" id={12} />     
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "IDX MAP"   grouping = "MISC" id={13} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "SRVC LABEL"grouping = "MISC" id={14} />    
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "CONFIG"    grouping = "MISC" id={15} />        
-                </PluginPicker>
-                <PluginPalette                                                       sentInArray={pluginArray}>
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "REST API"  grouping = "RPC"  id={0} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "GRPC"      grouping = "RPC"  id={1} />    
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "PROMETHEUS"grouping = "RPC"  id={2} />
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "ETCD"      grouping = "DS"   id={3} /> 
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "REDIS"     grouping = "DS"   id={4} /> 
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "CASSANDRA" grouping = "DS"   id={5} /> 
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "CONSUL"    grouping = "DS"   id={6} /> 
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "LOGRUS"    grouping = "LOG"  id={7} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "LOG MNGR"  grouping = "LOG"  id={8} />  
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "STTS CHECK"grouping = "HTH"  id={9} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "PROBE"     grouping = "HTH"  id={10} />  
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "KAFKA"     grouping = "MISC" id={11} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "DATASYNC"  grouping = "MISC" id={12} />     
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "IDX MAP"   grouping = "MISC" id={13} />   
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "SRVC LABEL"grouping = "MISC" id={14} />    
-                    <DraggablePlugins  handlerFromParent={this.handleData}  pluginName = "CONFIG"    grouping = "MISC" id={15} />  
+                <div className="left-column-background">
+                <p>hello</p>
+                </div>
+                <div className="plugin-column">
+                    {this.state.sentInNames.map((sentInName, index) => {
+                        return (
+                            <PluginPicker 
+                                key={index} 
+                                sentInName={sentInName} 
+                                sentInArray={pluginPickedArray.subarray(Number(offsets[index]),Number(offsets[index+1]))}>
+                                {this.state.pluginNames.slice(Number(offsets[index]), Number(offsets[index+1])).map((pluginName, pluginNameIndex) => {
+                                    return (
+                                        <DraggablePlugins
+                                            pluginName={pluginName}
+                                            handlerFromParent={this.handleData}
+                                            id={pluginNameIndex+Number(offsets[index])}
+                                            key={pluginNameIndex+Number(offsets[index])} 
+                                        />
+                                    )
+                                })}     
+                            </PluginPicker>
+                        )
+                    })}
+                </div>
+                <PluginPalette                                                      
+                    sentInArray={pluginPickedArray}>
+                    {this.state.pluginNames.map((pluginName, pluginNameIndex) => {
+                        return (<DraggablePlugins
+                            pluginName={pluginName}
+                            key={pluginNameIndex}
+                            id={pluginNameIndex}
+                            handlerFromParent={this.handleData}
+                        />)
+                    })}   
                 </PluginPalette>
             </div>
         );
