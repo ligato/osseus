6e54463ef9a6664991f6e9368990179f3dede36b
Sabrina <sctsui@ucsc.edu>
2019-04-09 15:52:15 -0700

    incomplete handler for loadAllPlugins WIP

--- plugins/restapi/handlers.go
+++ plugins/restapi/handlers.go
@@ -44,9 +44,13 @@ type Plugins struct{
 // Registers REST handlers
 func (p *Plugin) registerHandlersHere() {
 
-	// maybe change to /v1/projects/{id} for save
+	//save project state
 	p.HTTPHandlers.RegisterHTTPHandler("/v1/projects", p.SaveProjectHandler, POST)
+	//load project state for project with name = {id}
 	p.HTTPHandlers.RegisterHTTPHandler("/v1/projects/{id}", p.LoadProjectHandler, GET)
+	//load all projects
+	p.HTTPHandlers.RegisterHTTPHandler("/v1/projects", p.LoadAllProjectsHandler, GET)
+	//save project plugins to generate code
 	p.HTTPHandlers.RegisterHTTPHandler("/v1/templates/{id}", p.GenerateHandler, POST)
 
 }
@@ -97,7 +101,23 @@ func (p *Plugin) LoadProjectHandler(formatter *render.Render) http.HandlerFunc {
 	}
 }
 
-//registers handler for demo/generate endpoint
+func (p *Plugin) LoadAllProjectsHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		projectsInfo, err := p.LoadAllProjects()
+		if err != nil{
+			errMsg := fmt.Sprintf("500 Internal server error: request failed: %v\n", err)
+			p.Log.Error(errMsg)
+			p.logError(formatter.JSON(w, http.StatusInternalServerError, errMsg))
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		projectsJson := json.NewEncoder(w).Encode(projectsInfo)
+		p.logError(formatter.JSON(w, http.StatusOK, projectsJson))
+	}
+}
+
+//registers handler for generate endpoint
 func (p *Plugin) GenerateHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 
@@ -134,15 +154,23 @@ func (p *Plugin) GetServerStatus() (interface{}, error) {
 
 //save project
 func (p *Plugin) SaveProject(response Response) (interface{}, error) {
-	p.Log.Debug("REST API post /v1/projects plugin reached")
+	p.Log.Debug("REST API post /v1/projects save project reached")
 	p.genUpdater(response, projectsPrefix, response.ProjectName)
 	return response, nil
 }
 
 //get project from given id
 func (p *Plugin) LoadProject(projectId string) (interface{}, error) {
-	p.Log.Debug("REST API Get /v1/projects/{id} load plugin reached with id: ", projectId)
+	p.Log.Debug("REST API Get /v1/projects/{id} load project reached with id: ", projectId)
 	projectValue := p.getValue(projectsPrefix, projectId)
+	return projectValue, nil
+}
+
+//get project from given id
+func (p *Plugin) LoadAllProjects() (interface{}, error) {
+	p.Log.Debug("REST API Get /v1/projects load all projects reached")
+	projectValue := p.getAllValues(projectsPrefix)
+	p.Log.Debug("multiple projects?", projectValue)
 	return projectValue, nil
 }
 
@@ -231,6 +259,50 @@ func (p *Plugin) getValue(prefix string, key string) interface{} {
 
 	return project
 }
+
+// todo or note: not completely impl yet; list keys thinks there's no keys
+// todo possible thing to try is if the prefix isn't correct with the slash at the end
+// todo also the thing with kv.GetValue needs to be in some proto structure
+// todo so maybe a response struct
+func (p *Plugin) getAllValues(prefix string) interface{} {
+
+	prefix = "/projects/v1/plugins"
+	broker := p.KVStore.NewBroker(prefix)
+
+	keys, err := broker.ListKeys(prefix)
+
+	if err != nil {
+		p.Log.Errorf("GetValue failed: %v", err)
+	}
+
+	for {
+		key, val, all := keys.GetNext()
+		if all == true {
+			p.Log.Debug("AAAAAAAAAAA")
+			break
+		}
+
+		p.Log.Infof("Key: %q Val: %v", key, val)
+	}
+
+	/*resp, err := broker.ListValues("myproject")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		kv, stop := resp.GetNext()
+		if stop {
+			p.Log.Debug("stop")
+			break
+		}
+		p.Log.Debug("key is, ", kv.GetKey())
+		kv.GetValue()
+		p.Log.Debug("what is kv even", kv)
+	}
+	*/
+	return nil
+}
+
 
 // logError logs non-nil errors from JSON formatter
 func (p *Plugin) logError(err error) {
